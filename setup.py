24c243afe4a76824075baaa902aba487973ac02b
goldcvt <thegoldcat5@gmail.com>
2020-09-30 16:13:20 +0300

    Updated download_url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author = 'Leo',                   # Type in your name
     author_email='thegoldcat5@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/goldcvt/crawler_tools',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/goldcvt/crawler_tools/v_01.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/goldcvt/archive/crawler_tools/v_0.2.tar.gz',    # I explain this later on
     keywords = ['scraping', 'web-scraping', 'PostgreSQL'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
         'psycopg2'
